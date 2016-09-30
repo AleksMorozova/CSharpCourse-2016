3a8d39ef42e551b7c883843e78021c110113e52a
mrSmileG <golovko.greg@gmail.com>
2016-10-01 02:49:55 +0300

    Task 1 has already complete

---
+++ Task_1_Basics/2_8_Lottery/Cube.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_8_Lottery
+{
+    class Cube : ICube
+    {
+        private readonly int grainsCount;
+        public Cube(int grainsCount)
+        {
+            this.grainsCount = grainsCount;
+        }
+        public int Throw()
+        {
+            return Generator.Generate(1, grainsCount);
+        }
+    }
+}

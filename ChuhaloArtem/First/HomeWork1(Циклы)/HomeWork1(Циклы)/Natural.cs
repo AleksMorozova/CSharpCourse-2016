38f9f8a668c98bf8c56b49cb2a1d8d586b19784f
ChuhaloArtem <tema10021996@yandex.ru>
2016-10-12 21:47:07 +0300

    first

---
+++ ChuhaloArtem/First/HomeWork1(Циклы)/HomeWork1(Циклы)/Natural.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork1_Циклы_
+{
+    class Natural
+    {
+        public void Show()
+        {
+            int i=1;
+            Console.Write("\nEnter max item: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            while(i < number)
+            {
+                Console.Write("\n {0} ", Math.Pow(i,2));
+                i++;
+            }
+        }
+    }
+}
---
+++ ChuhaloArtem/First/HomeWork1(Циклы)/HomeWork1(Циклы)/Task3.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork1_Циклы_
+{
+    class Task3
+    {
+        public void Ugadai()
+        {
+            Random rnd = new Random();
+
+            int number = rnd.Next(1,146);
+            int step;
+
+            do
+            {
+                Console.Write("\nУгадывайте: ");
+                step = Convert.ToInt32(Console.ReadLine());
+
+                if (step < number)
+                    Console.WriteLine("Нужно больше.");
+                if (step > number)
+                    Console.WriteLine("Перебор.");
+            } while (step != number);
+            Console.WriteLine("\nВы угадали!");
+        }
+    }
+}

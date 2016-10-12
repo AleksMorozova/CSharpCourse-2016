0f02459e3062505d24d443976786d36013c38775
ChuhaloArtem <tema10021996@yandex.ru>
2016-10-12 21:33:24 +0300

    first commit

---
+++ HomeWork1(Циклы)/HomeWork1(Циклы)/Rozriad.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork1_Циклы_
+{
+    class Rozriad
+    {
+        public void Print()
+        {
+            double output=0;
+            Console.Write("\nEnter item: ");
+            long item = Convert.ToInt32(Console.ReadLine());
+
+            while (item != 0)
+            {
+                output++;
+                item /= 10;
+            }
+            Console.WriteLine("{0}", output);
+        }
+    }
+}

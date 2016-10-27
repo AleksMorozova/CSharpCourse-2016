d044bdac8ac5a3ac676f9d9bb4187f97a6d17883
PavelPisotskiy <pisockiypavel@gmail.com>
2016-10-27 23:16:22 +0300

    Added test cases

--- PisotskiyPavel/Task_5/Task_5_4/Program.cs
+++ PisotskiyPavel/Task_5/Task_5_4/Program.cs
@@ -13,8 +13,9 @@ namespace Task_5_4
             string str = "H__e__l_l_o W_o_r_l_d!!!";
             Console.WriteLine("Слово: {0}{1}", str, Environment.NewLine);
             Console.WriteLine("Индекс знака {0}: {1}", "'W'", StringExtensions.IndexOf(str, "W"));
-            //Console.WriteLine(StringExtensions.Replace(str, "_", "___"));
+            Console.WriteLine(StringExtensions.Replace(str, "_", "___"));
             Console.WriteLine(StringExtensions.Replace(str, "__", "_"));
+            Console.WriteLine(StringExtensions.Replace(str, "_", ""));
             Console.WriteLine(StringExtensions.Substring(str, str.Length - 12, 9));
 
             Console.ReadLine();

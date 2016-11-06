9ccced06b81fafb0fbe5534809e7f81de7e7e573
Nikita Maltsev <nikitamalcev1@gmail.com>
2016-11-06 14:31:32 +0200

    Tast7 completed

---
+++ maltsev/Task7/Delegate_1/Delegate_1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegate_1
+{ 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<int, int, int, double> mydel1 = delegate (int a, int b, int c)
+             {
+                 double d = ((double)a + (double)b + (double)c) / 3;
+                 return d;
+             };
+
+            Console.WriteLine("Delegate| Average of 2,10,55: {0:#.##} ", mydel1(2, 10, 55) );
+
+            Func<int, int, int, double> mydel2 = ( (a, b, c) => ((double)a + (double)b + (double)c) / 3 );
+              
+            Console.WriteLine("Lambda| Average of 2,10,55: {0:#.##} ", mydel1(2, 10, 55));
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ maltsev/Task7/Delegate_2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegate_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<int, int, int> Add= ((a, b) => a + b);
+            Func<int, int, int> Sub = ((a, b) => a - b);
+            Func<int, int, int> Mul = ((a, b) => a * b);
+            Func<int, int, double> Div = ((a, b) =>
+            {
+                try
+                {
+                    double c = a / b;
+                }
+                catch (DivideByZeroException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    return 0;
+                }
+                return (double)a / (double)b;
+            }); 
+
+            Console.WriteLine("{0:#.##}", Add(5, 6));
+            Console.WriteLine("{0:#.##}", Sub(5, 6));
+            Console.WriteLine("{0:#.##}", Mul(5, 6));
+            Console.WriteLine("{0:#.##}", Div(50, 6));
+            Console.WriteLine("{0:#.##}", Div(5, 0));
+
+            Console.ReadKey();
+
+        }
+    }
+}

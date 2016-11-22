50303338832798339cfb6930e52e8c1698ac1b57
Nikita Maltsev <nikitamalcev1@gmail.com>
2016-11-22 21:03:02 +0200

    Tast9 completed

---
+++ maltsev/Task9/Collections_2/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collections_2
+{
+    class MyList<T>: IEnumerable<T>
+    {
+        private T[] elements;
+
+        public int Capacity { get; private set; }
+
+        public T this[int index]
+        {
+            get { return elements[index]; }
+            set { elements[index] = value; }
+        }
+
+        public MyList()
+        {
+            Capacity = 0;
+        }
+        public void Add(T elem)
+        {
+            T[] elements2 = new T[Capacity + 1];
+            for (int i = 0; i < Capacity; i++)
+            {
+                elements2[i] = elements[i];
+            }
+            elements = elements2;
+            elements[Capacity] = elem;
+            Capacity++;
+        }
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = 0; i < elements.Length; i++)
+            {
+                yield return elements[i];
+            }
+        }
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<double> list = new MyList<double>();
+            list.Add(234.1);
+            list.Add(2222.11);
+            list.Add(343.222);
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ maltsev/Task9/Collections_3/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collections_3
+{
+    class MyDictionary<TKey, TValue> : IEnumerable<KeyValuePair<TKey, TValue>>  where TKey : IComparable
+    {
+        private TKey[] keys;
+        private TValue[] values;
+
+        public int Capacity { get; private set; }
+
+        public TValue this[TKey key]
+        {
+            get
+            {
+                for (int i = 0; i < keys.Length; i++)
+                {
+                    if (keys[i].Equals(key))
+                        return values[i];
+                }
+                return default(TValue);
+            }
+            set
+            {
+                for (int i = 0; i < keys.Length; i++)
+                {
+                    if (keys[i].Equals(key))
+                        values[i] = value;
+                }
+            }
+        }
+
+        public MyDictionary()
+        {
+            Capacity = 0;
+        }
+        public void Add(TKey key, TValue value)
+        {
+            TKey[] keys2 = new TKey[Capacity + 1];
+            TValue[] values2 = new TValue[Capacity + 1];
+
+            for (int i = 0; i < Capacity; i++)
+            {
+                keys2[i] = keys[i];
+                values2[i] = values[i];
+            }
+
+            keys = keys2;
+            keys[Capacity] = key;
+
+            values = values2;
+            values[Capacity] = value;
+
+            Capacity++;
+        }
+        public IEnumerator<KeyValuePair<TKey, TValue>> GetEnumerator()
+        {
+            for (int i = 0; i < values.Length; i++)
+            {
+                yield return new KeyValuePair<TKey, TValue>(keys[i], values[i]);
+            }
+        }
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, int> dic = new MyDictionary<int, int>();
+
+            dic.Add(2, 190);
+            dic.Add(5, 2990);
+            dic.Add(6, 12345);
+
+            Console.WriteLine(dic[2]);
+            Console.WriteLine(dic[5]);
+            Console.WriteLine(dic[6]);
+
+            foreach (var item in dic)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine(dic.Capacity);
+
+            Console.ReadKey();
+            }
+    }
+}

16804ca33687f0fa239117a8f2c34fb6461bf2ae
PavelPisotskiy <pisockiypavel@gmail.com>
2016-12-04 22:45:49 +0200

    My Exception is created

---
+++ PisotskiyPavel/MyExceptionTask/MyExceptionTask/NegativeNumberException.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyExceptionTask
+{
+    class NegativeNumberException : Exception
+    {
+        public NegativeNumberException()
+            : base()
+        {
+        }
+
+        public NegativeNumberException(String message)
+            : base(message)
+        {
+        }
+
+        public NegativeNumberException(String message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+
+        protected NegativeNumberException(SerializationInfo info, StreamingContext context)
+            : base(info, context)
+        {
+        }
+
+    }
+}
---
+++ PisotskiyPavel/MyExceptionTask/MyExceptionTask/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyExceptionTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите число для определения факториала: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            try
+            {
+                int factorial = Factorial(number);
+                Console.WriteLine($"{number}! = {factorial}");
+            }
+            catch (NegativeNumberException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+
+            Console.ReadLine();
+        }
+
+        static int Factorial(int number)
+        {
+            if (number < 0)
+                throw new NegativeNumberException("Negative number not permitted.");
+
+            int factorial = 1;
+
+            if (number == 0)
+                return factorial;
+
+            for (int i = 1; i <= number; i++)
+            {
+                factorial *= i;
+            }
+
+            return factorial;
+        }
+    }
+}

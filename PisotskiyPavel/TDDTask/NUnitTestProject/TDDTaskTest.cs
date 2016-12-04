640bf714c5db56f424e1f4ac5d993c4d132f7dee
PavelPisotskiy <pisockiypavel@gmail.com>
2016-12-04 22:57:02 +0200

    TDD task is done

---
+++ PisotskiyPavel/TDDTask/NUnitTestProject/TDDTaskTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using NUnit.Framework;
+using TDDTask;
+
+namespace NUnitTestProject
+{
+    [TestFixture]
+    public class TDDTaskTest
+    {
+        [Test]
+        public void SumTest()
+        {
+            Calculator calc = new Calculator();
+            double result = calc.Sum(10, -15);
+            double expected = -5;
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void SubtractTest()
+        {
+            Calculator calc = new Calculator();
+            double result = calc.Subtract(10, -15);
+            double expected = 25;
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void MultiplyTest()
+        {
+            Calculator calc = new Calculator();
+            double result = calc.Multiply(10, -15);
+            double expected = -150;
+
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
---
+++ PisotskiyPavel/TDDTask/TDDTask/Calculator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDDTask
+{
+    public class Calculator
+    {
+        public double Sum(double a, double b)
+        {
+            return a + b;
+        }
+
+        public double Subtract(double a, double b)
+        {
+            return a - b;
+        }
+
+        public double Multiply(double a, double b)
+        {
+            return a * b;
+        }
+    }
+}

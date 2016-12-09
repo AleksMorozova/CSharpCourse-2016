dcffed07d634ccda086dc99e7ae65df48fa19cd8
ChuhaloArtem <tema10021996@yandex.ru>
2016-12-09 11:19:17 +0200

    Task 7 + Technology

---
+++ ChuhaloArtem/7/HomeWork7(Task2)/HomeWork7(Task2)/Operation.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork7_Task2_
+{
+    class Operation
+    {
+        public static IntOperation Add = (a, b) => Console.WriteLine("A + B = {0}", a + b);
+        public static IntOperation Sub = (a, b) => Console.WriteLine("A - B = {0}", a - b);
+        public static IntOperation Mul = (a, b) => Console.WriteLine("A * B = {0}", a * b);
+        public static IntOperation Div = (a, b) =>
+        {
+            try
+            {
+                if (b == 0)
+                {
+                    throw new DivideByZeroException();
+                }
+                Console.WriteLine("A / B = {0}", a / b);
+            }
+            catch (DivideByZeroException e)
+            {
+                Console.WriteLine("Divide by zero exception. {0}", e.Message);
+            }
+        };
+    }
+}

16e3e12b9b316c4d7a52639c9b6ff843d14a10a3
PavelPisotskiy <pisockiypavel@gmail.com>
2016-12-16 21:02:22 +0200

    MessageMVVM

---
+++ PisotskiyPavel/MessageMVVM/MessageMVVM/MainWindowViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace MessageMVVM
+{
+    class MainWindowViewModel
+    {
+        RelayCommand openWindowCommand;
+
+        public ICommand OpenWindowCommand
+        {
+            get
+            {
+                if (openWindowCommand == null)
+                    openWindowCommand = new RelayCommand(OpenWindowCommandExecute);
+                return openWindowCommand;
+            }
+        }
+
+        private void OpenWindowCommandExecute(object obj)
+        {
+            Messenger.Default.Send<Message, string>(Message.ShowNewWindow, "Супер секретные данные =)");
+        }
+    }
+}
